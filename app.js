7e439d9ab1c0898c845ad14ed490b39316e597d3
vedavyas <vedavyas14042003@gmail.com>
2023-01-11 20:54:17 +0530

    adding email validation

--- app.js
+++ app.js
@@ -10,7 +10,27 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
-const select=document.querySelectorAll(".hidden");
-select.forEach((ele)=>{
-    observer.observe(ele)
-})
+const select = document.querySelectorAll(".hidden");
+select.forEach((ele) => {
+  observer.observe(ele);
+});
+
+const email = document.getElementById("email");
+const submit = document.getElementById("submit");
+const errorMsg = document.getElementById("error");
+const form=document.querySelector('form')
+const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+form.addEventListener("submit", (e) => {
+  let msg = [];
+  if (email.value===''|| email.value==null) {
+    msg.push('Email is reqiured');
+  }
+  if (email.value !== validRegex) {
+    msg.push('Check your email please')
+  }
+  if (msg.length > 0) {
+    e.preventDefault();
+    errorMsg.innerText = msg.join(",");
+  }
+});
